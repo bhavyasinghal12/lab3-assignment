3127856e06e867ec074136746d3b27355e7d140d
bhavyasinghal12 <31274681+bhavyasinghal12@users.noreply.github.com>
2017-09-15 01:33:07 +0530

    Update p9.cpp

--- p9.cpp
+++ p9.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+  char ch='0';
+  cout<<"Enter the character ";
+  cin>>ch;
+  if(((ch>=65)&&(ch<=90))||((ch>=97)&&(ch<=122)))
+    cout<<endl<<"the character is an alphabet";
+  else if((ch>=48)&&(ch<=57))
+    cout<<endl<<"The character is a digit";
+  else
+    cout<<endl<<"The character is a special character";
+  return 0;
+}
