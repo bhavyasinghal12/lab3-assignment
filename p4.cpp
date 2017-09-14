0481ea1a311fb2ac2d5be8512e966d3256bd8c47
bhavyasinghal12 <31274681+bhavyasinghal12@users.noreply.github.com>
2017-09-15 01:06:06 +0530

    Update p4.cpp

--- p4.cpp
+++ p4.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+int a=0,b,c;
+cout<<" enter a number"<<endl;
+cin>>a;
+b=a%5;
+c=a%11;
+if(b==0 && c==0)
+cout<<endl<<" number is divisible by 5 and 11";
+else
+cout<<endl<<" number is not divisible by 5 and 11";
+  return 0;
+}
