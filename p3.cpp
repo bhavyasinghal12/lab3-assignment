9d3a08513cfd6354d83972425e101e328ca5ab9b
bhavyasinghal12 <31274681+bhavyasinghal12@users.noreply.github.com>
2017-09-15 00:40:53 +0530

    Create p3.cpp

---
+++ p3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+float a;
+cout<<" enter a number"<<endl;
+cin>>a;
+if(a>0)
+{cout<<" the number is positive";}
+else if(a<0)
+{cout<<" the number is negative";}
+else if(a==0)
+{cout<<" the number is zero";}
+
+  return 0;
+}
