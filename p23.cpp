4c585282bcb4d85764d0650b41aab2f8a3baca35
bhavyasinghal12 <31274681+bhavyasinghal12@users.noreply.github.com>
2017-09-15 03:03:54 +0530

    Create p23.cpp

---
+++ p23.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ int main()
+ {int i,n;
+ cout<<" Enter a number";
+ cin>>n;
+ i=n;
+ cout<<endl;
+ while(i>0)
+ {cout<<i<<endl;
+ i--;
+ }
+ return 0;
+ }
