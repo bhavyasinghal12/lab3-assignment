6b51bd3a93d4a80fc8a0fa6798937aaf44875648
bhavyasinghal12 <31274681+bhavyasinghal12@users.noreply.github.com>
2017-09-15 03:36:23 +0530

    Create p29.cpp

---
+++ p29.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i=1,j,n,sum=0;
+	cout<<"enter a number";
+	cin>>n;
+	cout<<"\nSum of all even numbers upto "<<n<<" is ";
+	
+	while(i<=(n+1)/2)
+	{j=2*i-1;
+             sum+=j;
+	i++;
+	}
+	cout<<sum;
+	return 0;
+}
