562afc96df1979c0cad6045bbc7e5ff0699f1b6b
bhavyasinghal12 <31274681+bhavyasinghal12@users.noreply.github.com>
2017-09-15 01:08:59 +0530

    Create p5.cpp

---
+++ p5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a=0;
+  cout<<endl<<"Enter the number ";
+  cin>>a;
+  if((a%2==0))
+    cout<<endl<<"The number is even ";
+  else
+    cout<<endl<<"The number is odd";
+  return 0;
+}
