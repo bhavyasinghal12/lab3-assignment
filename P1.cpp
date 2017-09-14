2ae4aa88d9eea128cf637420096e331bd56da293
bhavyasinghal12 <31274681+bhavyasinghal12@users.noreply.github.com>
2017-09-15 00:19:54 +0530

    Create P1.cpp

---
+++ P1.cpp
@@ -0,0 +1,16 @@
+#include <iosteam>
+using namespace std;
+int main()
+{
+ int a=0,b=0;
+cout<<"enter two numbers"<<endl;
+cin>>a >>b;
+if(a<b)
+{cout<<b<<" is greater than "<<a;}
+else if(b<a)
+{cout<<a<<" is greater than "<<b;}
+else if(b=a)
+{cout<<"please enter distinct numbers";}
+	return 0;
+
+}
