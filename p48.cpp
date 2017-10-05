9209fc0f2384bde822bc56db2c978f9578a4d641
bhavyasinghal12 <31274681+bhavyasinghal12@users.noreply.github.com>
2017-10-05 17:23:54 +0530

    Create p48.cpp

---
+++ p48.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num=0,tnum=0;
+  cout<<"\nENTER THE NUMBER: ";
+  cin>>num;
+  int j=2;
+  cout<<endl<<endl;
+  while(j<=num)
+  {
+  int count=0;
+  tnum=j;
+  for(int i=2;i<tnum;i++)
+  {
+   if(tnum%i==0)
+   count++;
+  }
+  if(count==0)
+    cout<<"\n"<<tnum<<" is a prime number.";
+  j++;
+  }
+  return 0;
+}
