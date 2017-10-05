6fc27928690f768c53596e322f67786cd817a102
bhavyasinghal12 <31274681+bhavyasinghal12@users.noreply.github.com>
2017-10-05 17:25:16 +0530

    Create p50.cpp

---
+++ p50.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num=0,count=0;
+  cout<<"\nENTER THE NUMBER: ";
+  cin>>num;
+  cout<<"\n \nThe prime factors are: ";
+  for(int j=2;j<=num;j++)
+  {
+   if(num%j==0)
+   { count=0;  
+     for(int i=2;i<j;i++)
+     {
+       if(j%i==0)
+       count++;
+     }
+     if(count==0)  
+     cout<<"\n"<<j;
+    }
+  }
+  return 0;
+}
